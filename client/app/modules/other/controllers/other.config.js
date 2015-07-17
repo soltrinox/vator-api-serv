796066817996a442772700e1974814755a106ec1
Dylan Rosario <soltrinox@gmail.com>
2015-07-17 00:42:00 -0700

    fixed route

--- client/app/modules/other/controllers/other.config.js
+++ client/app/modules/other/controllers/other.config.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('com.module.other')
   .run(function($rootScope, gettextCatalog) {
-    $rootScope.addMenu(gettextCatalog.getString('Other'), 'app.other.other',
+    $rootScope.addMenu(gettextCatalog.getString('Other'), 'app.other',
       'fa-file-o');
     // $rootScope.addDashboardBox(gettextCatalog.getString('Other'), 'bg-maroon',
     //    'ion-information', 0, 'app.other.index');
