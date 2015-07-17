6a471408b412e993c83b043aeb748650507942af
Dylan Rosario <soltrinox@gmail.com>
2015-07-17 09:53:19 -0700

    more fields

--- client/app/modules/profile/controllers/profile.ctrl.js
+++ client/app/modules/profile/controllers/profile.ctrl.js
@@ -22,17 +22,39 @@ app.controller('MyProfileCtrl', function($scope, $state, $stateParams, ProfileSe
     }];
 
 
-    $scope.formFields2 = [ {
+    $scope.formFields2 = [{
+      key: "Degree",
+      type: "select",
+      label: "Type",
+      required: true,
+      options: [
+          {
+              name: "School",
+              value : "EDU"
+          },
+          {
+              name: "Military",
+              value : "MIL"
+          },
+          {
+              name: "Organization",
+              value : "ORG"
+          }
+      ]
+    }, {
         key: 'School',
         type: 'text',
         label: 'Name',
         required: true
-      }, {
-        key: 'Date',
-        type: 'text',
-        label: 'Year Graduated',
-        required: true
-      }, {
+      },{
+        "key": "Date",
+        "type": "number",
+        "label": "Year Completed",
+        "default": 2015,
+        "min": 1900,
+        "max": 2020,
+        "required": true
+    }, {
         key: 'Major',
         type: 'text',
         label: 'Field/Major',
@@ -47,26 +69,127 @@ app.controller('MyProfileCtrl', function($scope, $state, $stateParams, ProfileSe
         type: 'hidden',
         label: gettextCatalog.getString('UUID'),
         required: true
-      },{
-        key: "Degree",
+      }];
+
+
+  //     "companyname": "",
+  //  "jobtitle": "",
+  //  "datestart": "",
+  //  "dateend": "",
+  //  "id": "objectid",
+  //  "profileId": "objectid"
+
+      $scope.formFields3 = [{
+        key: "Type",
         type: "select",
         label: "Type",
         required: true,
         options: [
             {
-                name: "School",
-                value : "EDU"
+                name: "Employee",
+                value : "001"
             },
             {
-                name: "Military",
-                value : "MIL"
+                name: "Founder",
+                value : "002"
             },
             {
-                name: "Organization",
-                value : "ORG"
+                name: "Investor",
+                value : "003"
             }
         ]
-      }];
+      }, {
+          key: 'companyname',
+          type: 'text',
+          label: 'Company',
+          required: true
+        }, {
+          key: 'datestart',
+          type: 'text',
+          label: 'Start Date',
+          required: true
+        }, {
+          key: 'dateend',
+          type: 'text',
+          label: 'End Date',
+          required: false
+        },{
+          key: 'jobtitle',
+          type: 'text',
+          label: 'Role',
+          required: false
+        },{
+          key: 'profileId',
+          type: 'hidden',
+          label: gettextCatalog.getString('UUID'),
+          required: true
+        }];
+
+        // Type (string, optional),
+        // Value (string, optional),
+        // URL (string, optional),
+        // created (string, optional),
+        // status (number, optional),
+        // verified (boolean, optional),
+        // profileId (objectid, optional)
+
+
+        $scope.formFields4 = [ {
+          key: "Type",
+          type: "select",
+          label: "Type",
+          required: true,
+          options: [
+              {
+                  name: "Email",
+                  value : "001"
+              },
+              {
+                  name: "Facebook",
+                  value : "002"
+              },
+              {
+                  name: "Twitter",
+                  value : "003"
+              },
+              {
+                  name: "GitHub",
+                  value : "004"
+              },
+              {
+                  name: "LinkedIn",
+                  value : "005"
+              },
+              {
+                  name: "Google",
+                  value : "006"
+              }
+          ]
+        },{
+            key: 'Value',
+            type: 'text',
+            label: 'username / acct#',
+            required: true
+          }, {
+            key: 'created',
+            type: 'hidden',
+            required: true
+          }, {
+            key: 'status',
+            type: 'hidden',
+            default : 1,
+            required: true
+          },{
+            key: 'verified',
+            type: 'text',
+            default : false,
+            required: true
+          },{
+            key: 'profileId',
+            type: 'hidden',
+            label: gettextCatalog.getString('UUID'),
+            required: true
+          }];
 
   $scope.formOptions = {
     uniqueFormId: true,
@@ -81,6 +204,43 @@ app.controller('MyProfileCtrl', function($scope, $state, $stateParams, ProfileSe
   };
 
   $scope.onSubmit = function() {
+    ProfileService.upsertProfile($scope.profile, function() {
+      $scope.profiles = ProfileService.getProfiles();
+      $state.go('^.view');
+    });
+  };
+
+  $scope.delete2 = function(id) {
+    ProfileService.deleteProfile(id, function() {
+      $scope.profile = ProfileService.getProfile();
+    });
+  };
+
+  $scope.onSubmit2 = function() {
+    ProfileService.upsertProfile($scope.profile, function() {
+      $scope.profiles = ProfileService.getProfiles();
+      $state.go('^.view');
+    });
+  };
+
+  $scope.delete3 = function(id) {
+    ProfileService.deleteProfile(id, function() {
+      $scope.profile = ProfileService.getProfile();
+    });
+  };
+  $scope.onSubmit3 = function() {
+    ProfileService.upsertProfile($scope.profile, function() {
+      $scope.profiles = ProfileService.getProfiles();
+      $state.go('^.view');
+    });
+  };
+
+  $scope.delete4 = function(id) {
+    ProfileService.deleteProfile(id, function() {
+      $scope.profile = ProfileService.getProfile();
+    });
+  };
+  $scope.onSubmit4 = function() {
     ProfileService.upsertProfile($scope.profile, function() {
       $scope.profiles = ProfileService.getProfiles();
       $state.go('^.view');
