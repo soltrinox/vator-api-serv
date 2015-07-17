f79b47f60e4c71bfbc7526514e5dff5673ac45e7
Dylan Rosario <soltrinox@gmail.com>
2015-07-17 14:16:09 -0700

    try

--- client/app/modules/profile/services/profile.service.js
+++ client/app/modules/profile/services/profile.service.js
@@ -12,7 +12,7 @@ function($state, CoreService, Profile, User, Education, Social, WorkHistory, get
   this.getProfile = function(id, cb) {
 
     console.log('Profile.getEntireProfile() : ' + id);
-     return Profile.findById({id:id});
+     $scope.profile = Profile.findById({id:id});
 
   };
 
