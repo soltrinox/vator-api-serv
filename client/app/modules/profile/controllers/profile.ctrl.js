21153bc80bb858823c8c8f4c50adc90047dde9c5
Dylan Rosario <soltrinox@gmail.com>
2015-07-17 07:44:54 -0700

    formly

--- client/app/modules/profile/controllers/profile.ctrl.js
+++ client/app/modules/profile/controllers/profile.ctrl.js
@@ -1,80 +1,100 @@
 'use strict';
 var app = angular.module('com.module.profile', ['formly', 'formlyBootstrap']);
 
-app.controller('MyProfileCtrl', function($scope, $state, $stateParams, User, ProfileService) {
+app.controller('MyProfileCtrl', function($scope, $state, $stateParams, User, ProfileService, formlyVersion) {
 
     $scope.pid = $stateParams.id;
     console.log('PID: '+$scope.pid);
 
     var vm = this;
-        // funcation assignment
-        vm.onSubmit = onSubmit;
-
-        // variable assignment
-        vm.author = { // optionally fill in your info below :-)
-          name: 'Kent C. Dodds',
-          url: 'https://twitter.com/kentcdodds'
-        };
-        vm.exampleTitle = 'Default Value'; // add this
-        vm.env = {
-          angularVersion: angular.version.full,
-          formlyVersion: formlyVersion
-        };
-
-        vm.model = {
-          lastName: 'Smith'
-        };
-        vm.options = {};
-
-        vm.fields = [
-          {
-            key: 'firstName',
-            type: 'input',
-            defaultValue: 'This is a default value',
-            templateOptions: {
-              label: 'First Name (initialized via default value)'
-            }
-          },
-          {
-            key: 'lastName',
-            type: 'input',
-            defaultValue: 'This is a default value',
-            templateOptions: {
-              label: 'Last Name (initialized via the model)'
-            }
-          },
-          {
-            key: 'candy',
-            type: 'select',
-            defaultValue: 'milky_way',
-            templateOptions: {
-              label: 'Favorite Candy (initialized via default value',
-              options: [
-                {name: 'Snickers', value: 'snickers'},
-                {name: 'Baby Ruth', value: 'baby_ruth'},
-                {name: 'Milky Way', value: 'milky_way'}
-              ]
-            }
-          },
-          {
-            key: 'agree',
-            type: 'checkbox',
-            templateOptions: {
-              label: 'Agree? (not initialized at all)',
-              required: true
-            }
-          }
-        ];
-
-        vm.originalFields = angular.copy(vm.fields);
 
 
-        // function definition
-        function onSubmit() {
-          vm.options.updateInitialValue();
-          alert(JSON.stringify(vm.model), null, 2);
-        }
-      });
+  // variable assignment
+  vm.author = { // optionally fill in your info below :-)
+    name: 'Kent C. Dodds',
+    url: 'https://twitter.com'
+  };
+  vm.exampleTitle = 'Formly Bootstrap Select'; // add this
+  vm.env = {
+    angularVersion: angular.version.full,
+    formlyVersion: formlyVersion
+  };
+
+  vm.model = {};
+  vm.options = {};
+
+  vm.fields = [
+    {
+      key: 'marvel1',
+      type: 'select',
+      templateOptions: {
+        label: 'Normal Select',
+        options: [
+          {name: 'Iron Man', value: 'iron_man'},
+          {name: 'Captain America', value: 'captain_america'},
+          {name: 'Black Widow', value: 'black_widow'},
+          {name: 'Hulk', value: 'hulk'},
+          {name: 'Captain Marvel', value: 'captain_marvel'}
+        ]
+      }
+    },
+    {
+      key: 'marvel2',
+      type: 'select',
+      templateOptions: {
+        label: 'Grouped Select',
+        options: [
+          {name: 'Iron Man', value: 'iron_man', group: 'Male'},
+          {name: 'Captain America', value: 'captain_america', group: 'Male'},
+          {name: 'Black Widow', value: 'black_widow', group: 'Female'},
+          {name: 'Hulk', value: 'hulk', group: 'Male'},
+          {name: 'Captain Marvel', value: 'captain_marvel', group: 'Female'}
+        ]
+      }
+    },
+    {
+      key: 'marvel3',
+      type: 'select',
+      templateOptions: {
+        label: 'Select with custom name/value/group',
+        options: [
+          {label: 'Iron Man', id: 'iron_man', gender: 'Male'},
+          {label: 'Captain America', id: 'captain_america', gender: 'Male'},
+          {label: 'Black Widow', id: 'black_widow', gender: 'Female'},
+          {label: 'Hulk', id: 'hulk', gender: 'Male'},
+          {label: 'Captain Marvel', id: 'captain_marvel', gender: 'Female'}
+        ],
+        groupProp: 'gender',
+        valueProp: 'id',
+        labelProp: 'label'
+      }
+    },
+    {
+      key: 'marvel3',
+      type: 'select',
+      templateOptions: {
+        label: 'Custom ng-options',
+        options: [
+          {label: 'Iron Man', id: 'iron_man', gender: 'Male'},
+          {label: 'Captain America', id: 'captain_america', gender: 'Male'},
+          {label: 'Black Widow', id: 'black_widow', gender: 'Female'},
+          {label: 'Hulk', id: 'hulk', gender: 'Male'},
+          {label: 'Captain Marvel', id: 'captain_marvel', gender: 'Female'}
+        ],
+        ngOptions: 'option as option.label group by option.gender for option in to.options'
+      }
+    }
+  ];
+
+  vm.originalFields = angular.copy(vm.fields);
+
+  function vm.onSubmit() {
+      vm.options.updateInitialValue();
+      alert(JSON.stringify(vm.model), null, 2);
+    }
+
+
+
 
 
   $scope.delete = function(id) {
