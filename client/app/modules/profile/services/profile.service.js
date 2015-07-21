29e4a28b91c1a5d30ec1615bd22f180ffa3f041f
Dylan Rosario <soltrinox@gmail.com>
2015-07-21 14:26:17 -0700

    getEntireProfile

--- client/app/modules/profile/services/profile.service.js
+++ client/app/modules/profile/services/profile.service.js
@@ -11,8 +11,8 @@ function($state, CoreService, Profile, User, Education, Social, WorkHistory, get
 
   this.getProfile = function(id, cb) {
 
-    
-     return Profile.findById({id:id});
+    return Profile.getEntireProfile({id:id});
+  //   return Profile.findById({id:id});
 
   };
 
