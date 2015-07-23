f12bb9acfc34c4615186035e1f1ed3a6dd52d4c6
Dylan Rosario <soltrinox@gmail.com>
2015-07-23 09:51:01 -0700

    scope

--- client/app/modules/profile/config/profile.config.js
+++ client/app/modules/profile/config/profile.config.js
@@ -4,7 +4,7 @@ var app = angular.module('com.module.profile');
 app.run(function($rootScope,  Profile, gettextCatalog) {
 
 
-  console.log('currentUser ID : ' + $scope.currentUser.id );
+
 
   $rootScope.addMenu(gettextCatalog.getString('Profile'), 'app.myprofile.list',
     'fa-rocket');
