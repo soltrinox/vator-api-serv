8801afc6d880a400eae4e6c7711d0b1d9c012e7d
Dylan Rosario <soltrinox@gmail.com>
2015-07-23 12:47:12 -0700

    $scope.SelectedProfile

--- client/app/modules/profile/services/profile.service.js
+++ client/app/modules/profile/services/profile.service.js
@@ -11,7 +11,7 @@ function($state, CoreService, Profile, User, Education, Social, WorkHistory, get
 
   this.getProfile = function(id) {
     console.log('getPro : '+id);
-    return   Profile.getEntireProfile({id:id});
+    return Profile.getEntireProfile({id:id});
   };
 
   this.upsertProfile = function(profile, cb) {
