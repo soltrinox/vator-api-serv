e95bad9a2e322c55bf023f2a975e89e61e3094b8
Dylan Rosario <soltrinox@gmail.com>
2015-07-23 09:47:03 -0700

    checking ID

--- client/app/modules/profile/config/profile.config.js
+++ client/app/modules/profile/config/profile.config.js
@@ -1,7 +1,11 @@
 'use strict';
 var app = angular.module('com.module.profile');
 
-app.run(function($rootScope, Profile, gettextCatalog) {
+app.run(function($rootScope, $scope, $location, $state, $stateParams, Profile, gettextCatalog) {
+
+
+  console.log('currentUser ID : ' + $scope.currentUser.id );
+
   $rootScope.addMenu(gettextCatalog.getString('Profile'), 'app.myprofile.list',
     'fa-rocket');
   //
