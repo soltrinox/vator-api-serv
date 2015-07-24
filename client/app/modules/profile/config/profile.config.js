0395ee45693a23ca7ebb634803e485758b64616c
Dylan Rosario <soltrinox@gmail.com>
2015-07-24 16:22:32 -0700

    form

--- client/app/modules/profile/config/profile.config.js
+++ client/app/modules/profile/config/profile.config.js
@@ -6,7 +6,7 @@ app.run(function($rootScope,  Profile, gettextCatalog) {
 
 
 
-  $rootScope.addMenu(gettextCatalog.getString('Profile'), 'app.myprofile.form',
+  $rootScope.addMenu(gettextCatalog.getString('Profile'), 'app.myprofile',
     'fa-rocket');
   //
   // Profile.find(function(data) {
