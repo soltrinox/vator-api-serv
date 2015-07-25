6bdd02977194db2068716203f50fc15962032540
Dylan Rosario <soltrinox@gmail.com>
2015-07-25 16:43:19 -0700

    response.data

--- client/app/modules/profile/controllers/profile.ctrl.js
+++ client/app/modules/profile/controllers/profile.ctrl.js
@@ -367,7 +367,7 @@ app.controller('MyProfileCtrl',function($scope, $location, $state, $route, $rout
       }
     }).then(function(response){
       console.log('Company : ' + JSON.stringify(response));
-      return response(function(item){
+      return response.data(function(item){
         return item.Name;
       });
     });
