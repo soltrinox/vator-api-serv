e9fddac03c75f48f815e95c4a15b3e412aa67423
Dylan Rosario <soltrinox@gmail.com>
2015-07-27 22:27:44 -0700

    test tagging

--- client/app/modules/products/controllers/products.ctrl.js
+++ client/app/modules/products/controllers/products.ctrl.js
@@ -126,4 +126,21 @@ angular.module('com.module.products')
       });
     };
 
+    $scope.tags = [
+       { name: "Brazil", flag: "Brazil.png" },
+       { name: "Italy", flag: "Italy.png" },
+       { name: "Spain", flag: "Spain.png" },
+       { name: "Germany", flag: "Germany.png" },
+     ];
+
+     $scope.loadCountries = function($query) {
+       return $http.get('/test.json', { cache: true}).then(function(response) {
+         var countries = response.data;
+         return countries.filter(function(country) {
+           return country.name.toLowerCase().indexOf($query.toLowerCase()) != -1;
+         });
+       });
+     };
+
+
   });
