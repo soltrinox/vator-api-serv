4a40234879e9c686d5f30f9bdb4b0559ddb3de10
Dylan Rosario <soltrinox@gmail.com>
2015-07-29 14:01:23 -0700

    fx

--- client/app/modules/about/services/about.service.js
+++ client/app/modules/about/services/about.service.js
@@ -1,20 +1,10 @@
 'use strict';
 var app = angular.module('com.module.about');
 
-app.service('AboutService',  ['$state', 'CoreService', 'ProfileService', 'AppAuth' 'Profile', 'User',
+app.service('AboutService',  ['$state', 'CoreService', 'ProfileService', 'AppAuth', 'Profile', 'User',
 'keys', 'Categories', 'Tags', 'Graphs',  'gettextCatalog',
 function($state, CoreService, Profile, User, Education, Social, WorkHistory, Team, gettextCatalog) {
 
-
-
-  this.upsertCategory = function(catItem, cb) {
-    var response = Categories.upsert(catItem, function() {
-        console.log('CAT DATA UPSERT : '+JSON.stringify(response));
-      cb(response);
-    }, function(err) {
-      console.log('This cat could not be saved: ') + err);
-    });
-  };
 
 
 
