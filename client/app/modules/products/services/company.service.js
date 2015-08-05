8b4e305950c3e5d680271615471040d847f2cefd
Dylan Rosario <soltrinox@gmail.com>
2015-08-05 09:31:58 -0700

    uploading

---
+++ client/app/modules/products/services/company.service.js
@@ -0,0 +1,7 @@
+'use strict';
+var app = angular.module('com.module.product');
+
+app.service('CompanyService', ['$state', 'CoreService', 'Product','Profile', 'User','Team',  'gettextCatalog',
+function($state, CoreService, Product, Profile, User, Team, gettextCatalog) {
+    
+}]);
