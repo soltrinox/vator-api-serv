8b9d558edc6cd78299c0821b388b71527a1966a3
Dylan Rosario <soltrinox@gmail.com>
2015-08-05 22:55:08 -0700

    pic

--- client/app/modules/products/controllers/products.ctrl.js
+++ client/app/modules/products/controllers/products.ctrl.js
@@ -325,6 +325,7 @@ angular.module('com.module.products')
                 }
             }).then(function(response){
               return response.data.map(function(item){
+                console.log('PIC: '+ item.ProfilePic);
                 return item.Name;
               });
             });
