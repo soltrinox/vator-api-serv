df7ed5cce1558d59bad6c2acb14c92bbab5f6a8b
Dylan Rosario <soltrinox@gmail.com>
2015-08-07 14:40:34 -0700

    rt

--- client/app/modules/core/config/core.routes.js
+++ client/app/modules/core/config/core.routes.js
@@ -1,32 +1,6 @@
 'use strict';
 angular.module('com.module.core')
   .config(function($stateProvider, $urlRouterProvider, stateHelperProvider) {
-
-    // stateHelperProvider
-    //         .state({
-    //             name: 'router',
-    //             template: '<div class="lockscreen" style="height: 100%"></div>',
-    //             controller : 'RouteCtrl'
-    //         })
-    //         .state({
-    //             name: 'app',
-    //             templateUrl: 'modules/core/views/app.html',
-    //             controller: 'MainCtrl',
-    //             children: [ {
-    //                 name: 'app.home',
-    //                 url: '',
-    //                 templateUrl: 'modules/core/views/home.html',
-    //                 controller: 'HomeCtrl'
-    //               }
-    //             ]
-    //         })
-    //         .state({
-    //             name: 'rootSibling',
-    //             templateUrl: 'rootSibling.html'
-    //         });
-    //
-    //
-
     $stateProvider
       .state('router', {
         url: '/router',
@@ -43,11 +17,34 @@ angular.module('com.module.core')
         url: '',
         templateUrl: 'modules/core/views/home.html',
         controller: 'HomeCtrl'
-      })
-      .state('app.x', {
-        url: '/x',
-        templateUrl: 'modules/core/views/xhome.html',
-        controller: 'XCtrl'
       });
     $urlRouterProvider.otherwise('/router');
+
+
+
+        // stateHelperProvider
+        //         .state({
+        //             name: 'router',
+        //             template: '<div class="lockscreen" style="height: 100%"></div>',
+        //             controller : 'RouteCtrl'
+        //         })
+        //         .state({
+        //             name: 'app',
+        //             templateUrl: 'modules/core/views/app.html',
+        //             controller: 'MainCtrl',
+        //             children: [ {
+        //                 name: 'app.home',
+        //                 url: '',
+        //                 templateUrl: 'modules/core/views/home.html',
+        //                 controller: 'HomeCtrl'
+        //               }
+        //             ]
+        //         })
+        //         .state({
+        //             name: 'rootSibling',
+        //             templateUrl: 'rootSibling.html'
+        //         });
+        //
+        //
+
   });
