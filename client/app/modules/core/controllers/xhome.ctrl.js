6cca111a3cc164f8b3f0f414d10015223c986d7c
Dylan Rosario <soltrinox@gmail.com>
2015-08-14 13:37:40 -0700

    confirm

--- client/app/modules/core/controllers/xhome.ctrl.js
+++ client/app/modules/core/controllers/xhome.ctrl.js
@@ -7,7 +7,7 @@
  * @requires $rootScope
  **/
 angular.module('com.module.core')
-  .controller('XCtrl', function($scope, $route, $rootScope) {
+  .controller('XCtrl', function($scope, $route, $rootScope, CoreService, User, gettextCatalog) {
 
     $scope.count = {};
 
@@ -20,5 +20,28 @@ angular.module('com.module.core')
 
         // $route.reload();
     });
+
+    $scope.confirmXUpgrade = function(){
+
+      CoreService.confirm('This is an agreement', 'So do you agree?',
+        function() {
+          $scope.currentUser.vatorX = 'valid';
+          $scope.currentUser = User.save($scope.currentUser,
+          function() {
+            $rootScope.isXsession = true;
+            $rootScope.masterUser = $scope.currentUser;
+            CoreService.alert('You agree!');
+          },
+          function(res) {
+            CoreService.toastError(gettextCatalog.getString(
+              'Error registering!'), res.data.error.message);
+            $scope.registerError = res.data.error;
+            }
+          );
+        },
+        function() {
+          CoreService.alert('You don\'t agree!');
+        });
+    };
 
   });
